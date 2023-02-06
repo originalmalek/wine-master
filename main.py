4fe69fae8643c94cf99fb4753fea009bbbba163d
originalmalek <originalmalek@gmail.com>
2023-02-06 08:20:37 -0300

    .env variable

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 import pandas as pd
+from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from datetime import datetime
+
 
 
 def convert_year_text(work_years):
@@ -15,8 +18,8 @@ def convert_year_text(work_years):
 		return 'лет'
 
 
-def create_product_dict():
-	data_frame = pd.read_excel(io='products.xlsx',
+def create_product_dict(products_file):
+	data_frame = pd.read_excel(io=products_file,
 	                           sheet_name='Лист1',
 	                           names=['category', 'title', 'sort', 'price', 'image', 'offer'],
 	                           keep_default_na=False,
@@ -26,13 +29,13 @@ def create_product_dict():
 	return products_dict
 
 
-def render_page():
+def render_page(products_file):
 	template = env.get_template('template.html')
 	work_years = int((datetime.now() - datetime(year=1920, month=1, day=1)).days / 365)
 	rendered_page = template.render(
 		year=work_years,
 		year_text=convert_year_text(work_years),
-		products_data=create_product_dict(),
+		products_data=create_product_dict(products_file),
 	)
 
 	with open('index.html', 'w', encoding="utf8") as file:
@@ -40,10 +43,14 @@ def render_page():
 
 
 if __name__ == '__main__':
+	load_dotenv()
+	products_file = os.getenv('PRODUCTS_FILE')
+	print(products_file)
 	env = Environment(
 		loader=FileSystemLoader('.'),
 		autoescape=select_autoescape(['html', 'xml'])
 	)
-	render_page()
+
+	render_page(products_file)
 	server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 	server.serve_forever()
